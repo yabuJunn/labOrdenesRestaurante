0b02eebe5b0a5d4465a1f56f705afcd44c299287
yabuJunn <juancho04reyes@gmail.com>
2024-09-22 18:00:57 -0500

    Created Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import './App.css'
 
 //Components
 import { Title } from './Components/Title/Title'
+import { Menu } from './Components/Menu/Menu'
 
 function App() {
 
   return (
     <>
       <Title></Title>
+
+      <div id='menuAndResultContainer'>
+        <Menu></Menu>
+      </div>
     </>
   )
 }
---
+++ src/Components/Menu/Menu.tsx
@@ -0,0 +1,7 @@
+export const Menu = () => {
+    return <>
+        <div id="MenuContainer">
+            <h2>Menu Options</h2>
+        </div>
+    </>
+}
