be6a50b4e20f18f5c4bf2aa0204a8172a1f79915
yabuJunn <juancho04reyes@gmail.com>
2024-09-23 14:20:46 -0500

    Added styles to TotalItem component

--- src/Components/TotalItem/TotalItem.tsx
+++ src/Components/TotalItem/TotalItem.tsx
@@ -1,5 +1,11 @@
 import './TotalItem.css'
 
+//Import Imagenes
+
+import addImg from '../../assets/svg/plus.svg'
+import substractImg from '../../assets/svg/minus.svg'
+import deleteImg from '../../assets/svg/trash.svg'
+
 interface TotalItemProps {
     itemName: string,
     itemPrice: number,
@@ -10,13 +16,19 @@ export const TotalItem = ({ itemName, itemPrice, itemCuantity }: TotalItemProps)
     return <>
         <div className='TotalItemContainer'>
             <div className='TotalItemText'>
-                <p className='itemName'>{itemName} - {itemPrice}</p>
+                <p className='itemName'>{itemName} - ${itemPrice}</p>
                 <p className='itemPrice'>Cantidad: ${itemCuantity} - ${itemCuantity * itemPrice}</p>
             </div>
 
             <div className='TotalItemButtonContainer'>
-                <button>
-
+                <button id='addButton' className='TotalItemButton'>
+                    <img src={addImg} alt="" />
+                </button>
+                <button id='subtractButton' className='TotalItemButton'>
+                    <img src={substractImg} alt="" />
+                </button>
+                <button id='deleteButton' className='TotalItemButton'>
+                    <img src={deleteImg} alt="" />
                 </button>
             </div>
         </div>
